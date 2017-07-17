6f374e8ee060ae40a1ef5ab485250efa23d1db3b
Pan <panfree23@gmail.com>
2017-07-17 14:29:47 +0800

    init

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,8 +1,7 @@
 <template>
 	<div class="app-wrapper" :class="{hideSidebar:!sidebar.opened}">
-		<div class="sidebar-wrapper">
-			<sidebar class="sidebar-container"></sidebar>
-		</div>
+			<sidebar class="sidebar-wrapper"></sidebar>
+
 		<div class="main-container">
 			<navbar></navbar>
 			<app-main></app-main>
@@ -28,53 +27,98 @@
   }
 </script>
 
-<style rel="stylesheet/scss" lang="scss" scoped>
-	@import "src/styles/mixin.scss";
-	.app-wrapper {
-		@include clearfix;
-		position: relative;
-		height: 100%;
-		width: 100%;
-		&.hideSidebar {
-			.sidebar-wrapper {
-				transform: translate(-140px, 0);
-				.sidebar-container {
-					transform: translate(132px, 0);
+<style rel="stylesheet/scss" lang="scss">
+@import "src/styles/mixin.scss";
+.app-wrapper {
+	@include clearfix;
+	position: relative;
+	height: 100%;
+	width: 100%;
+	&.hideSidebar {
+		.sidebar-wrapper {
+			// transform: translate(-140px, 0);
+			width: 50px;
+			.el-icon-arrow-down {
+				// display: none!important;
+				opacity: 0;
+			}
+			&:hover {
+				transform: translate(0, 0);
+			}
+			.submenu-title,
+			.submenu-title-noDropdown {
+				display: none!important;
+			}
+			.el-menu{
+				height: 0px;
+				display: none;
+			}
+		}
+		.submenu-item {
+
+			&:hover {
+
+				.submenu-title-noDropdown {
+					display: block!important;
+					background: #1f2d3d;
+					position: absolute;
+					left: 50px;
+					top: 0px;
+					padding: 0px 20px;
 				}
-				&:hover {
-					transform: translate(0, 0);
-					.sidebar-container {
-						transform: translate(0, 0);
+				.el-menu {
+					display: block!important;
+					position: absolute;
+					height: auto;
+					left: 50px;
+					top: 0px;
+					overflow: hidden;
+				}
+				&.el-submenu{
+					.el-submenu{
+						.submenu-title{
+							display: block!important;
+						}
+						.el-icon-arrow-down{
+							opacity: 1;
+						}
+						.el-menu{
+							height: 0px;
+							position: relative;
+							left: 0;
+						}
+						&.is-opened{
+								.el-menu{
+										height: auto;
+										display: block!important;
+								}
+						}
 					}
 				}
 			}
-			.main-container {
-				margin-left: 40px;
-			}
-		}
-		.sidebar-wrapper {
-			width: 180px;
-			position: fixed;
-			top: 0;
-			bottom: 0;
-			left: 0;
-			z-index: 1001;
-			overflow: hidden;
-			transition: all .28s ease-out;
-		}
-		.sidebar-container {
-			transition: all .28s ease-out;
-			position: absolute;
-			top: 0;
-			bottom: 0;
-			left: 0;
-			right: -17px;
-			overflow-y: scroll;
 		}
 		.main-container {
-			min-height: 100%;
-			transition: all .28s ease-out;
-			margin-left: 180px;
+			margin-left: 40px;
 		}
 	}
+	.submenu-item {
+	display: block;
+	position: relative;
+	}
+	.sidebar-wrapper {
+		width: 180px;
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		z-index: 1001; // overflow: hidden;
+		transition: all .28s ease-out;
+	}
+	.main-container {
+		min-height: 100%;
+		transition: all .28s ease-out;
+		margin-left: 180px;
+	}
+}
+
 </style>
--- src/views/layout/SidebarItem.vue
+++ src/views/layout/SidebarItem.vue
@@ -1,21 +1,22 @@
 <template>
 	<div>
 		<template v-for="item in routes">
-			<router-link v-if="!item.hidden&&item.noDropdown&&item.children.length>0" :to="item.path+'/'+item.children[0].path">
-				<el-menu-item :index="item.path+'/'+item.children[0].path">
-					<icon-svg v-if='item.icon' :icon-class="item.icon"></icon-svg>{{item.children[0].name}}
-				</el-menu-item>
+			<router-link class='submenu-item' v-if="!item.hidden&&item.noDropdown&&item.children.length>0" :to="item.path+'/'+item.children[0].path">
+				<span :index="item.path+'/'+item.children[0].path">
+					<icon-svg v-if='item.icon' :icon-class="item.icon"></icon-svg><span class='submenu-title-noDropdown'>{{item.children[0].name}}</span>
+				</span>
 			</router-link>
-			<el-submenu :index="item.name" v-if="!item.noDropdown&&!item.hidden">
+
+			<el-submenu class='submenu-item' :index="item.name" v-if="!item.noDropdown&&!item.hidden">
 				<template slot="title">
-					<icon-svg v-if='item.icon' :icon-class="item.icon"></icon-svg>{{item.name}}
+					<icon-svg v-if='item.icon' :icon-class="item.icon"></icon-svg><span class='submenu-title'>{{item.name}}</span>
 				</template>
 				<template v-for="child in item.children" v-if='!child.hidden'>
 					<sidebar-item class='menu-indent' v-if='child.children&&child.children.length>0' :routes='[child]'> </sidebar-item>
-					<router-link v-else class="menu-indent" :to="item.path+'/'+child.path">
-						<el-menu-item :index="item.path+'/'+child.path">
+					<router-link  v-else class="menu-indent" :to="item.path+'/'+child.path">
+						<span :index="item.path+'/'+child.path">
 							{{child.name}}
-						</el-menu-item>
+						</span>
 					</router-link>
 				</template>
 			</el-submenu>
